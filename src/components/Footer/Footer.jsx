6623abaaf581fd163acfb4386e07df29633cb182
vaibhav09981 <vaibhavjj99@gmail.com>
2025-07-24 10:55:39 +0530

    New file has been created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css"
+function Footer() {
+  return (
+    <div className='footer' >
+      Made With ❤️ by Vaibhav Jamloki
+    </div>
+  )
+}
+
+export default Footer
